611945ad9649ae3d3ef3b7eb66f92967eee8e8b9
Vlad <satogob75@gmail.com>
2025-06-03 13:06:04 +0600

    set

--- car_rental_bank/settings.py
+++ car_rental_bank/settings.py
@@ -167,4 +167,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:5174"]
