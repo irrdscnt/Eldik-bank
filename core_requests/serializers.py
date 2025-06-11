43146d7c2d91138556523b646e2ce617f7dfb061
Vlad <satogob75@gmail.com>
2025-06-11 17:23:10 +0600

    .

--- core_requests/serializers.py
+++ core_requests/serializers.py
@@ -4,7 +4,7 @@ from bson import ObjectId
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
-
+from core_requests.models import *
 
 class TripSerializer(serializers.Serializer):
     id = serializers.CharField(read_only=True)
