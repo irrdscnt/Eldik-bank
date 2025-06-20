e25a0acfefd2fdeeb6ecada95f175e6b79c2e787
Vlad <satogob75@gmail.com>
2025-06-20 14:28:30 +0600

    add url

--- car_rental_bank/settings.py
+++ car_rental_bank/settings.py
@@ -209,7 +209,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://nominatim.openstreetmap.org"]
 
 CHANNEL_LAYERS = {
     "default": {
